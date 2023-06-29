289221e18f5ca11aa5435c12a33e20cd05007015
sleklere <santiagoleklere@gmail.com>
2023-06-28 23:33:13 -0300

    protect middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,19 @@
 const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
 const User = require('../models/User');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
-exports.protect = (req, res, next) => {};
-
 const signToken = id =>
-  jwt.sign({ id }, process.env.JWT_SECRET, process.env.JWT_EXPIRES_IN);
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   const cookieOptions = {
     expires: new Date(
-      Date.now() + process.env.JWT_EXPIRES_IN * 1000 * 60 * 60 * 24
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
   };
@@ -76,3 +77,32 @@ exports.logout = (req, res) => {
   });
   res.status(200).json({ status: 'success' });
 };
+
+exports.protect = catchAsync(async (req, res, next) => {
+  const authHeaders = req.headers.authorization;
+  let token;
+
+  if (authHeaders && authHeaders.startsWith('Bearer')) {
+    token = authHeaders.split(' ')[1];
+  } else if (req.cookies?.jwt) {
+    token = req.cookies.jwt;
+  }
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in. Please login to access.', 401)
+    );
+  }
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  const currentUser = await User.findById(decoded.id);
+
+  // check if current user still exists
+  if (!currentUser) {
+    return next('The user belonging to the token does no longer exist.', 401);
+  }
+
+  // conceed access
+  req.user = currentUser;
+  next();
+});
